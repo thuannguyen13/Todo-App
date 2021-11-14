2aefcb59810bd7ce142f97c0b3202a1607d01af0
Thuan Nguyen <nguyenducthuan133@gmail.com>
2021-11-14 23:27:59 +0700

    todo app intial

--- src/js/script.js
+++ src/js/script.js
@@ -1,24 +1,22 @@
-var api = 'https://reqres.in/api/users';
+// var api = 'https://reqres.in/api/users';
 
-var btn,
-    txtArea;
+// var btn,
+//     txtArea;
 
-btn     = document.querySelector("#test")
-txtArea = document.querySelector("#txtArea")
+// btn     = document.querySelector("#test")
+// txtArea = document.querySelector("#txtArea")
 
-
-
-function triggerAlert(){
-    console.log(btn)
-    var users;
-    var values = []
-    users = fetch(api)
-            .then(res => res.json())
-            .then(data => {
-                for (const iDs in data.data ) {
-                    values.push(data.data[iDs].email)
-                };
-                txtArea.value = values.join("\r\n")
-                console.log(values)
-            });
-};
+// function triggerAlert(){
+//     console.log(btn)
+//     var users;
+//     var values = []
+//     users = fetch(api)
+//             .then(res => res.json())
+//             .then(data => {
+//                 for (const iDs in data.data ) {
+//                     values.push(data.data[iDs].email)
+//                 };
+//                 txtArea.value = values.join("\r\n")
+//                 console.log(values)
+//             });
+// };
